abb055c1d0e0e1bc0a3962f2a3f0ba5304721ea2
Jason Pham <61170796+jasonhtpham@users.noreply.github.com>
2021-05-25 19:16:50 +1000

    Add addStory form to dashboard

--- src/views/dependants/Dashboard/Dashboard.js
+++ src/views/dependants/Dashboard/Dashboard.js
@@ -1,40 +1,61 @@
 import React from 'react';
-import { Box, Container } from '@material-ui/core';
+import { Box, Container, TextField, Grid, Typography, Paper, FormControl, InputLabel, Select, MenuItem, makeStyles, createStyles } from '@material-ui/core';
 import { LayoutConfig } from 'constants/index';
+// import { classes } from '../../../../node_modules/coa/coa';
 // import {StoryArAPI} from 'helpers';
 
-// const useStyles = makeStyles(theme => createStyles({
-//   root: {
-//     padding: theme.spacing(2),
-//     margin: theme.spacing(2),
-//     width: "50%"
-//   }
-// }));
+const useStyles = makeStyles(theme => createStyles({
+  root: {
+    padding: theme.spacing(2),
+  },
+  formControl: {
+    minWidth: 120,
+  },
+}));
 
 export const Dashboard = () => {
-  /*
   const classes = useStyles();
 
-  // Declare data hook
-  const [story, setStory] = React.useState([]);
+  const [aim, setAim] = React.useState(null);
 
-  // A function getting data by calling the API
-  const prepData = React.useCallback (() => {
-    StoryArAPI.getStories()
-      .then( response => setStories(response.data.data))
-      .catch(err => {
-        console.log(err);
-      });
-  },[]);
+  const handleChange = (event) => {
+    setAim(event.target.value);
+  };
 
-  const postData = React.useCallback (() => {
-    StoryArAPI.getStories()
-      .then( response => setStories(response.data.data))
-      .catch(err => {
-        console.log(err);
-      });
-  },[]);
-  */
+  
+
+  // const renderAssetInputs = () => {
+  //   for (let i = 0 ; i < assetAmount ; i++) {
+  //     document.getElementById("assets").appendChild(
+  //       <React.Fragment>
+  //         <Grid item xs={12}>
+  //           <TextField
+  //             required
+  //             id={`assetType${i}`}
+  //             name="assetType"
+  //             label="Asset Type"
+  //             fullWidth
+  //           />
+  //         </Grid>
+  //         <Grid item xs={6} sm={6}>
+  //           <TextField
+  //             required
+  //             id={`asset${i}Lat`}
+  //             name="lat"
+  //             label="Latitude"
+  //           />
+  //         </Grid>
+  //         <Grid item xs={6} sm={6}>
+  //           <TextField
+  //             required
+  //             id={`asset${i}Long`}
+  //             name="long"
+  //             label="Longtitude"
+  //           />
+  //         </Grid>
+  //       </React.Fragment>);
+  //   }
+  // };
 
   return (<Box sx={LayoutConfig.defaultContainerSX}>
     <Container
@@ -51,9 +72,124 @@ export const Dashboard = () => {
         }
       }}
     >
+      <Paper className={classes.root}>
+        <Typography variant="h4" gutterBottom>
+          Add new story
+        </Typography>
+        <form>
+          <Grid container spacing={3}>
 
+            <Grid item xs={12} sm={12}>
+              <TextField
+                required
+                id="title"
+                name="title"
+                label="Story title"
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={12}>
+              <TextField
+                required
+                id="outlined-multiline-static"
+                label="Description"
+                multiline
+                rows={2}
+                fullWidth
+                placeholder="A brief description of the story"
+                variant="outlined"
+              />
+            </Grid>
+
+            <Grid item xs={6} sm={6}>
+              <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel id="aim">Aim</InputLabel>
+                <Select
+                  required
+                  labelId="aim"
+                  id="aim-select-outlined"
+                  value={aim}
+                  onChange={handleChange}
+                  label="Aim"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value="FindAll"> Find All </MenuItem>
+                  <MenuItem value="FindOne"> Find One </MenuItem>
+                  <MenuItem value="FindAny"> Find Any </MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControl variant="outlined" className={classes.formControl}>
+                <TextField
+                  required
+                  id="assetAmountInput"
+                  name="assetAmountInput"
+                  label="No. of assets"
+                  xs = {6}
+                  type="number"
+                  InputProps={{
+                    inputProps: { 
+                      max: 5, min: 1
+                    }
+                  }}
+                />
+              </FormControl>
+            </Grid>
+
+            {/* <Grid item xs={12}>
+              <TextField
+                id="address2"
+                name="address2"
+                label="Address line 2"
+                fullWidth
+                autoComplete="shipping address-line2"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="city"
+                name="city"
+                label="City"
+                fullWidth
+                autoComplete="shipping address-level2"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField id="state" name="state" label="State/Province/Region" fullWidth />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="zip"
+                name="zip"
+                label="Zip / Postal code"
+                fullWidth
+                autoComplete="shipping postal-code"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="country"
+                name="country"
+                label="Country"
+                fullWidth
+                autoComplete="shipping country"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              
+            </Grid> */}
+          </Grid>
+        </form>
+      </Paper>
       
-
     </Container>
   </Box>);
 };
