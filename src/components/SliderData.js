23d002fe9e56112f69338b6ee51c8f07cfcf3aee
Scott West <west55721@gmail.com>
2021-05-31 23:48:10 +1000

    website with carousel

---
+++ src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData =[
+    {
+        image: "https://innovate.ieee.org/wp-content/uploads/2018/05/VR-Brain-Interface-Cover.jpg"
+    },
+    {
+        image: "https://th.bing.com/th/id/R22deb44372b43e661efc560730ef876c?rik=iRimyPjOt%2blpUA&riu=http%3a%2f%2fwww.brihaspatitech.com%2fblog%2fwp-content%2fuploads%2faugmented_reality_1.jpg&ehk=D9ugg4wgawBvbyJEs7IoID1MpNpOb2kHeqjkskEjySY%3d&risl=&pid=ImgRaw"
+    },
+    {
+        image: 'https://www.hopetutors.com/wp-content/uploads/2019/05/Augmented-Reality.jpg'
+    },
+    {
+        image: 'https://www.techicy.com/wp-content/uploads/2019/09/Augmented-Reality.png'
+    }
+]
