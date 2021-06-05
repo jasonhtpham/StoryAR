97fee34a3304973e335268ba2b6baa34b9f33fb0
Jason Pham <61170796+jasonhtpham@users.noreply.github.com>
2021-06-05 16:35:08 +1000

    fix before_install script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/storyar-fe
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt install -y nodejs 
 apt install -g npm@7.5.4
